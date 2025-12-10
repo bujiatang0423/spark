519330f1f642db310d185cc089395dede44e21bc
bujiatang0423 <bujiatang0423@gmail.com>
2025-12-10 18:29:14 +0800

    spark项目框架

--- src/main/java/org/simple/spark/hello/HelloWord.java
+++ src/main/java/org/simple/spark/hello/HelloWord.java
@@ -1,7 +1,14 @@
 package org.simple.spark.hello;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.sql.SparkSession;
+import scala.Tuple2;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -18,8 +25,22 @@ public class HelloWord {
                 .getOrCreate();
 
         final JavaRDD<String> javaRDD = spark.sparkContext().textFile(filePath, 1).toJavaRDD();
+        final JavaRDD<String> wordRDD = javaRDD.flatMap(new FlatMapFunction<String, String>() {
+            @Override
+            public Iterator<String> call(String line) throws Exception {
+                return Arrays.asList(line.split(" ")).iterator();
+            }
+        });
 
-        javaRDD.take(5).forEach(System.out::println);
+        final JavaRDD<String> filter = wordRDD.filter(word -> !word.isEmpty());
+
+        final JavaPairRDD<String, Integer> pairRDD = filter.mapToPair(word -> new Tuple2<>(word, 1));
+
+        final JavaPairRDD<String, Integer> countRDD = pairRDD.reduceByKey(Integer::sum);
+
+        final List<Tuple2<Integer, String>> take = countRDD.mapToPair(tp -> new Tuple2<>(tp._2, tp._1)).sortByKey(false).take(5);
+
+        take.forEach(System.out::println);
 
         spark.stop();
     }
